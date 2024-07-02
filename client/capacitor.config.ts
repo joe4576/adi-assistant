27f98702bf0c0f1aa0f21bc41aa141c7ab524807
Joe Brown <joebrown.4576@gmail.com>
2024-07-02 21:57:03 +0100

    Add format command

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'client',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "client",
+  webDir: "dist",
 };
 
 export default config;
